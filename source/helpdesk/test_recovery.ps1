beab277569bf9b4a2af1445049ee3f4281f64900
hoatran <dev03@midomax.vn>
2026-07-25 14:38:18 +0700

    chore: don file backup, script chua chay va JAR build san khong con dung

--- source/helpdesk/test_recovery.ps1
+++
@@ -1,5 +0,0 @@
-$file = 'd:\Midomax\MIDOMAX PROJECT\helpdesk\helpdesk\src\main\resources\templates\dashboard.html'
-$text = [System.IO.File]::ReadAllText($file, [System.Text.Encoding]::UTF8)
-$bytes = [System.Text.Encoding]::UTF8.GetBytes($text)
-$restored = [System.Text.Encoding]::Default.GetString($bytes)
-$restored.Substring(19200, 500)
--- source/helpdesk/test_recovery3.ps1
+++
@@ -1,6 +0,0 @@
-$file = 'd:\Midomax\MIDOMAX PROJECT\helpdesk\helpdesk\src\main\resources\templates\dashboard.html'
-$text = [System.IO.File]::ReadAllText($file, [System.Text.Encoding]::UTF8)
-$encoding1252 = [System.Text.Encoding]::GetEncoding(1252)
-$bytes = $encoding1252.GetBytes($text)
-$restored = [System.Text.Encoding]::UTF8.GetString($bytes)
-[System.IO.File]::WriteAllText('d:\Midomax\MIDOMAX PROJECT\helpdesk\helpdesk\restored2.txt', $restored, [System.Text.Encoding]::UTF8)
